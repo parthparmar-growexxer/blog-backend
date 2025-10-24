0530c20cfdf23ea222c78427f055f9c66619c0e7
Parth Parmar <parth.parmar@growexx.com>
2025-10-24 18:37:35 +0530

    hotfix: SonarQube Code Smell Fixes.

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['localhost:3000', 'http://localhost:3000', 'http://127.0.0.1:3000', 'localhost', 'http://localhost', 'http://localhost:8080',],
+    'allowed_origins' => ['localhost:3000', 'http://localhost:3000', 'http://127.0.0.1:3000', 'localhost', 'http://localhost', 'http://localhost:8080', 'http://127.0.0.1:8080'],
 
     'allowed_origins_patterns' => [],
 
